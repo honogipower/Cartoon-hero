31a1e53666104dec31834a3471598dd75e8ea784
honogipower <mguilmette@live.com>
2022-06-28 18:23:25 -0400

    Created a css test html page and css file

---
+++ style-test_files/config.js
@@ -0,0 +1,6 @@
+
+window[window["TiktokAnalyticsObject"]].instance("C41INCKVE57P75FK46SG").setAdvancedMatchingAvailableProperties({"email":true,"phone_number":true,"auto_email":false,"auto_phone_number":false});
+window[window["TiktokAnalyticsObject"]].instance("C41INCKVE57P75FK46SG").setPixelInfo && window[window["TiktokAnalyticsObject"]].instance("C41INCKVE57P75FK46SG").setPixelInfo({status: 0, name: "Skillshare", advertiserID: "6847208126676992006", setupMode: 1, partner: "", is_onsite: false });
+window[window["TiktokAnalyticsObject"]].setPCMDomain && window[window["TiktokAnalyticsObject"]].setPCMDomain("skillshare.com");
+window[window["TiktokAnalyticsObject"]].setPCMConfig && window[window["TiktokAnalyticsObject"]].setPCMConfig([]);
+window[window["TiktokAnalyticsObject"]].enableFirstPartyCookie && window[window["TiktokAnalyticsObject"]].enableFirstPartyCookie(true);
